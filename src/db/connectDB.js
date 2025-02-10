09a302480301a9ea5c6f440bc5c1543f7bd1e775
Shanuj Yadav <shanujyadav@gmail.com>
2025-02-10 13:10:17 +0530

    connection db

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -2,8 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
     try {
-        let MONGOURI = 'mongodb+srv://marketing:w8ykukFQ3PVtdbn5@cluster0.rcfxd.mongodb.net/Cluster0'
-        const connectionInstance = await mongoose.connect(`${MONGOURI}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGOURI}`)
         console.log(`MongoDB Connected---- ${connectionInstance.connection.host}`);
     } catch (e) {
         console.log('Mongo DB Error ---', e)
